52a80ad9f17e0d3a107c6f56ae3a98ad2697ca5b
Juan Gonzalo Carcamo <jcarcamo@gmail.com>
2016-11-15 09:02:32 -0500

    Writing to a file, making loop computations to get certain steps.

--- vs/pgr3_diffusion_win/pgr3_diffusion_cuda_win/pgr3_diffusion_cuda.cu
+++ vs/pgr3_diffusion_win/pgr3_diffusion_cuda_win/pgr3_diffusion_cuda.cu
@@ -6,8 +6,8 @@
 
 #define THREADS_PER_BLOCK 32 //1024 total threads per block
 
-#define DELTA_X 1024 //This is a square of 1024
-#define DELTA_Y 1024 //    by 1024
+#define DELTA_X 128 //This is a square of 1024
+#define DELTA_Y 128 //    by 1024
 
 #define MAX_STEPS_KERNEL 10 //number of computations in kernel per cycle
 #define MIN_VARIATION 0.00005
@@ -27,7 +27,7 @@ __global__ void calc_diffuse(double *current_mat, double *prev_mat, size_t rows,
 
 		if ((row < rows) && (col < cols)) {
 			if (row == 0 && col == 0) {
-				current_mat[(row)*cols + (col)] = (100.0 + prev_mat[(row + 1)*cols + (row)] + 100 + prev_mat[(row)*cols + (col + 1)]) / 4;
+				current_mat[(row)*cols + (col)] = (100 + prev_mat[(row + 1)*cols + (row)] + 100 + prev_mat[(row)*cols + (col + 1)]) / 4;
 			}
 			else if (row == 0 && col <= cols - 1) {
 				current_mat[(row)*cols + (col)] = (23 + prev_mat[(row + 1)*cols + (row)] + prev_mat[(row)*cols + (col - 1)] + prev_mat[(row)*cols + (col + 1)]) / 4;
@@ -83,18 +83,37 @@ int init_t0(double *mat1, size_t rows, size_t cols, double K, double room_temp)
 	return 0;
 }
 
-void print_matrix(double *mat, size_t rows, size_t cols) {
+void write_matrix(double *mat, size_t rows, size_t cols) {
+	FILE *f = fopen("C:/xampp/htdocs/pgr3/data/data_t1.json", "w");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
 	int i;
 	int j;
-	printf("[");
+	fprintf(f, "{\"z\": [");
 	for (i = 0; i < rows; i++) {
-		printf("[");
+		fprintf(f, "[");
 		for (j = 0; j < cols; j++) {
-			printf(" %.5f ",mat[i*cols + j]);
+			if (j == cols - 1) {
+				fprintf(f, " %.5f ", mat[i*cols + j]);
+			}
+			else {
+				fprintf(f, " %.5f, ", mat[i*cols + j]);
+			}
+			
 		}
-		printf("]\n");
+		if (i == rows - 1) {
+			fprintf(f, "]\n");
+		}
+		else {
+			fprintf(f, "],\n");
+		}
+		
 	}
-	printf("]\n");
+	fprintf(f, "] }");
+	fclose(f);
 }
 
 int main() 
@@ -107,11 +126,11 @@ int main()
 	mat1 = (double*)calloc(DELTA_X * DELTA_Y,sizeof(double));
 	mat2 = (double*)calloc(DELTA_X * DELTA_Y,sizeof(double));
 	
-	init_t0(mat1, DELTA_X, DELTA_Y, 100.0, 23.0);
-	printf("MAT1\n");
+	init_t0(mat1, DELTA_X, DELTA_Y, 1000.0, 23.0);
+	
 	double *mat1_d, *mat2_d;
 	int *result_d;
-
+	printf("Allocating CUDA memory \n");
 	cudasafe(cudaMalloc ((void**) &result_d, sizeof(int)), "cudaMalloc(result) failed.");
 	cudasafe(cudaMemcpy (result_d,&stop, sizeof(int), cudaMemcpyHostToDevice), "cudaMemcpy host->dev (result) failed.");
 
@@ -130,12 +149,24 @@ int main()
 	long long blocks_y = ceil(DELTA_Y / ((float) block_size));
 	dim3 dimblock (THREADS_PER_BLOCK, THREADS_PER_BLOCK);
 	dim3 dimgrid (blocks_x, blocks_y);
-
-	calc_diffuse <<< dimgrid, dimblock >>>(mat2_d, mat1_d, DELTA_X, DELTA_Y, result_d);
-	debug_step << <1, 1 >> > (mat2_d, DELTA_X);
+	printf("Starting Kernel \n");
+	unsigned int time_steps;
+	for (time_steps = 0; time_steps < 1024 * 1024; time_steps++) {
+		if(time_steps%2 == 0){
+			calc_diffuse << < dimgrid, dimblock >> > (mat2_d, mat1_d, DELTA_X, DELTA_Y, result_d);
+		}
+		else {
+			calc_diffuse << < dimgrid, dimblock >> > (mat1_d, mat2_d, DELTA_X, DELTA_Y, result_d);
+		}
+		
+		debug_step <<<1, 1 >>> (mat2_d, DELTA_X);
+	}
+	printf("Kernel Finished \n");
 	cudasafe(cudaMemcpy(mat2, mat2_d, DELTA_X * DELTA_Y * sizeof(double), cudaMemcpyDeviceToHost), "cudaMemcpy host <-dev(mat2_d) failed.");
-	print_matrix(mat2, DELTA_X, DELTA_Y);
-	cudaDeviceSynchronize(); 
+	cudaDeviceSynchronize();
+	printf("Writing to file \n");
+	write_matrix(mat2, DELTA_X, DELTA_Y);
+	printf("Finished writing \n");
 	printf("Press any key to exit!");
 	getchar();
  
